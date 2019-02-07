17e43f3570d47645e3b1f91e14ae30910ffd051b
GideonBrasil <gidxx_z@yahoo.com>
2019-02-07 14:53:15 -0700

    Update callBacksIntro.js

--- callBacksIntro.js
+++ callBacksIntro.js
@@ -1,6 +1,6 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  //loop through the length of each element of the array  
+  //loop through the length of each element in the array  
   for (var i = 0; i < arr.length; i++) {
     // execute the callback if the string index of the array = "waldo"
     if (arr[i] === "Waldo") {
